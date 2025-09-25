909a85471b22a0e1a1507bf2af8215ac535782b3
JannatulNaima1278 <jannatulnaima20052005@gmail.com>
2025-09-26 02:01:15 +0600

    added footer

---
+++ home.js
@@ -0,0 +1,35 @@
+const sareeBtn = document.getElementById('sharee-btn');
+sareeBtn.addEventListener('click', function(){
+    'sharee.html'
+    window.location.href = './sharee.html';
+})
+
+const threePieceBtn = document.getElementById('three-piece-btn');
+threePieceBtn.addEventListener('click', function(){
+    'pakistani.html'
+    window.location.href = './pakistani.html';
+})
+
+const westernBtn = document.getElementById('western-btn');
+westernBtn.addEventListener('click', function(){
+    'western.html'
+    window.location.href = './western.html';
+})
+
+const shoeBtn = document.getElementById('shoe-btn');
+shoeBtn.addEventListener('click', function(){
+    'shoe.html'
+    window.location.href = './shoe.html';
+})
+
+const jewellaryBtn = document.getElementById('jewellary-btn');
+jewellaryBtn.addEventListener('click', function(){
+    'jewellary.html'
+    window.location.href = './jewellary.html';
+})
+
+const makeupBtn = document.getElementById('makeup-btn');
+makeupBtn.addEventListener('click', function(){
+    'makeup.html'
+    window.location.href = './makeup.html';
+})
